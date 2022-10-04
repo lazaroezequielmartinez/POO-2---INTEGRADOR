baafabc44a5c67b0a2ed9c87b06d2818582b7522
SilviaBeatrizRamirez <solecitogith@gmail.com>
2022-10-04 16:24:21 -0300

    controladores

---
+++ Ready! - Integrador - V1.0.3/minerva/src/main/java/com/poo2integrador/minerva/Controlador/AtencionCliente/AtencionClienteControlador.java
@@ -0,0 +1,24 @@
+package com.poo2integrador.minerva.Controlador.AtencionCliente;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/atencionCliente")
+@Controller
+public class AtencionClienteControlador {//carga pedidos
+
+    @GetMapping("/cargarPedido")
+    public String cargarPedidos(Model model){
+
+        return "AtenPublicoRegCli";
+    }
+
+    @PostMapping("/nuevoPedido")//registra nuevo pedido
+    public String nuevoPedido(){
+        return "AtenPublicoRegCli";
+    }
+}
