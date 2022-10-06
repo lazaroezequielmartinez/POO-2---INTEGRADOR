e3e55cc6ea05c24b98c784ae0caa536523f54e3d
SilviaBeatrizRamirez <solecitogith@gmail.com>
2022-10-06 18:09:20 -0300

    modificaciones

--- Ready! - Integrador - V1.0.3/minerva/src/main/java/com/poo2integrador/minerva/Controlador/AtenPublicRegCliControlador.java
+++ Ready! - Integrador - V1.0.3/minerva/src/main/java/com/poo2integrador/minerva/Controlador/AtenPublicRegCliControlador.java
@@ -1,13 +1,24 @@
 package com.poo2integrador.minerva.Controlador;
 
+import com.poo2integrador.minerva.Controlador.Pedidos.AtencionClienteControlador;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
-public class AtenPublicRegCliControlador {
 
-    @GetMapping("/AtencionPublico")
-    public String AtencionPublico() {
-        return "AtenPublicoEditCli";
+@Controller
+
+public class AtenPublicRegCliControlador {
+    @Autowired
+    AtenPublicEditCliControlador atenPublicRegCliControlador;
+    @Autowired
+    AtencionClienteControlador cliente;
+    public String AtenPublicRegCliControlador(AtenPublicEditCliControlador atenPublicRegCliControlador, AtenPublicEditCliControlador cliente) {
+        this.atenPublicRegCliControlador=atenPublicRegCliControlador;
+
+
+        return "";
     }
+  //@GetMapping("/Cliente")
+
 }
